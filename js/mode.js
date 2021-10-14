026925b7f4e68915c1fcaa8a0ac3e3c1a3d0dc1c
Quan <nguyendinhquan0804@gmail.com>
2021-10-15 01:25:59 +0700

    lam dark-light

---
+++ js/mode.js
@@ -0,0 +1,7 @@
+// document.getElementById("btnMode").onclick = function(){
+//     document.getElementById("myBody").classList.toggle("dark");
+// }
+
+document.getElementById("switchButton").onclick = function () {
+    document.getElementById("myBody").classList.toggle("dark");
+};
